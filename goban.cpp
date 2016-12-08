53783023697e40b2e56b3179c7dc2fb7bc1d08f3
David Tuckey <quilan.v@gmail.com>
2016-12-08 11:38:09 +0100

    Detection liberté, suicide et fonction seco,ndaire

--- goban.cpp
+++ goban.cpp
@@ -10,3 +10,62 @@ void goban::jouer(int x,int y)
     save = plateau;
 
 }
+
+bool goban::detecterLiberte( int x, int y ) {
+  int couleur = plateau[x][y];
+  bool liberte = false;
+  checked.push_back({x,y});
+  if ( x > 0) {
+    if ( plateau[x-1][y] == -1 ) {
+      liberte = true;
+    }
+    else if ( plateau[x-1][y] == couleur and isNotChecked(x-1,y)) {
+      liberte = liberte||detecterLiberte(x-1, y);
+    }
+  }
+  if ( y > 0) {
+    if ( plateau[x][y-1] == -1 ) {
+      liberte = true;
+    }
+    else if ( plateau[x][y-1] == couleur and isNotChecked(x,y-1)) {
+      liberte = liberte||detecterLiberte(x, y-1);
+    }
+  }
+  if ( x < taille-1) {
+    if ( plateau[x+1][y] == -1 ) {
+      liberte = true;
+    }
+    else if ( plateau[x+1][y] == couleur and isNotChecked(x+1,y)) {
+      liberte = liberte||detecterLiberte(x+1, y);
+    }
+  }
+  if ( y < taille-1) {
+    if ( plateau[x][y+1] == -1 ) {
+      liberte = true;
+    }
+    else if ( plateau[x][y+1] == couleur and isNotChecked(x,y+1)) {
+      liberte = liberte||detecterLiberte(x, y+1);
+    }
+  }
+
+  return liberte;
+
+}
+
+bool goban::isNotChecked(int x, int y) {
+  bool retour = true;
+  for ( int i=0; i<checked.size(); i++) {
+    if ( checked[i][0] == x and checked[i][1] == y) {
+      retour = false;
+    }
+  }
+  return retour;
+
+}
+
+bool goban::detecterSuicide( int x, int y ) {
+  if ( plateau[x][y] == -1)
+    return true;
+  else
+    return false;
+}
--- goban.h
+++ goban.h
@@ -11,19 +11,21 @@ private:
 
     int joueur; // noir 0, blanc 1
 
-    int[] capture[2]; // capture[0] capturé par les noirs, captu…
+    int[] capture[2]; // capture[0] capturï¿½ par les noirs, captuï¿½
 
     std::vector< int[2] > checked;
 
-    bool detecterLiberté( int x, int y,); // true si + de 1 degré, false si 0 degré
+    bool detecterLibertï¿½( int x, int y,); // true si + de 1 degrï¿½, false si 0 degrï¿½
 
     bool detecterSuicide( int x, int y ); // Regarde le tableau et renvoie true si il y a     un pion de la bonne couleur en x et y
 
     void supprimerGroupe( int x, int y ); // supprime le groupe qui a un pion en x et ypublic :
 
+    bool isNotChecked(int x, int y);
+
     goban( int taille );
 
-    void jouer( int x, int y); // Sauvegarde du tableau, on fait le mouvement sur le tableau plateau, les vérifs, si problème, on recharge la sauvegarde et on met un message d'erreur
+    void jouer( int x, int y); // Sauvegarde du tableau, on fait le mouvement sur le tableau plateau, les vï¿½rifs, si problï¿½me, on recharge la sauvegarde et on met un message d'erreur
 
     void afficher ();
 
