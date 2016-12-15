73c6c19e4928a2fb2d3a5b90e5d28016fd52f6f5
David Tuckey <quilan.v@gmail.com>
2016-12-15 09:55:22 +0100

    debut test

--- main_test.cpp
+++ main_test.cpp
@@ -1,5 +1,7 @@
 #include "goban.h"
 #include <gtest>
+
+
 int main() {
     goban *myGoban = new goban(10);
     myGoban->afficher();
@@ -17,10 +19,93 @@ TEST(ObjectTest, TestInitialisation) {
   EXPECT_EQ(myGoban->getCapture()[1], 0);
 
   for( int i=0; i<myGoban->getTaille(); i++) {
-    vector<int> temp;
-    plateau.push_back(temp);
     for ( int u=0; u<taille; u++) {
-      plateau[i].push_back(-1);
+	  	EXPECT_EQ(myGoban->getPlateau()[i][j], -1);
     }
   }
+}
+
+
+TEST(ObjectTestLiberte, TestPositions) {
+  goban *myGoban = new goban(10);
+	myGoban->getPlateau()[5][5] = 0;
+	myGoban->getPlateau()[0][5] = 0;
+	EXPECT_EQ(myGoban->detecterLiberte(5,5), true);
+	myGoban->getPlateau()[0][0] = 0;
+	EXPECT_EQ(myGoban->detecterLiberte(0,0), true);
+	myGoban->getPlateau()[10][10] = 0;
+	EXPECT_EQ(myGoban->detecterLiberte(10,10), true);
+}
+
+TEST(ObjectTestLiberte, TestBloque) {
+  goban *myGoban = new goban(10);
+	myGoban->getPlateau()[5][5] = 0;
+	myGoban->getPlateau()[4][5] = 1;
+	myGoban->getPlateau()[5][4] = 1;
+	myGoban->getPlateau()[5][6] = 1;
+	myGoban->getPlateau()[6][5] = 1;
+	EXPECT_EQ(myGoban->detecterLiberte(5,5), false);
+	myGoban->getPlateau()[0][0] = 0;
+	myGoban->getPlateau()[1][0] = 1;
+	myGoban->getPlateau()[0][1] = 1;
+	EXPECT_EQ(myGoban->detecterLiberte(0,0), false);
+	myGoban->getPlateau()[10][10] = 0;
+	myGoban->getPlateau()[9][10] = 1;
+	myGoban->getPlateau()[10][9] = 1;
+	EXPECT_EQ(myGoban->detecterLiberte(10,10), false);
+}
+
+TEST(ObjectTestLiberte, TestGroupeNonBloque) {
+  goban *myGoban = new goban(10);
+	myGoban->getPlateau()[5][5] = 0;
+	myGoban->getPlateau()[4][5] = 1;
+	myGoban->getPlateau()[5][4] = 1;
+	myGoban->getPlateau()[5][6] = 1;
+	myGoban->getPlateau()[6][5] = 0;
+	EXPECT_EQ(myGoban->detecterLiberte(5,5), true);
+	myGoban->getPlateau()[0][0] = 0;
+	myGoban->getPlateau()[1][0] = 0;
+	myGoban->getPlateau()[0][1] = 1;
+	EXPECT_EQ(myGoban->detecterLiberte(0,0), true);
+	myGoban->getPlateau()[10][10] = 0;
+	myGoban->getPlateau()[9][10] = 1;
+	myGoban->getPlateau()[10][9] = 0;
+	EXPECT_EQ(myGoban->detecterLiberte(10,10), true);
+}
+
+TEST(ObjectTestLiberte, TestGroupeBloque) {
+  goban *myGoban = new goban(10);
+	myGoban->getPlateau()[5][5] = 0;
+	myGoban->getPlateau()[4][5] = 1;
+	myGoban->getPlateau()[5][4] = 1;
+	myGoban->getPlateau()[5][6] = 1;
+	myGoban->getPlateau()[6][5] = 0;
+	myGoban->getPlateau()[6][4] = 1;
+	myGoban->getPlateau()[6][6] = 1;
+	myGoban->getPlateau()[7][5] = 1;
+	EXPECT_EQ(myGoban->detecterLiberte(5,5), false);
+	myGoban->getPlateau()[0][0] = 0;
+	myGoban->getPlateau()[1][0] = 0;
+	myGoban->getPlateau()[0][1] = 1;
+	myGoban->getPlateau()[1][1] = 1;
+	myGoban->getPlateau()[2][0] = 1;
+	EXPECT_EQ(myGoban->detecterLiberte(0,0), false);
+	myGoban->getPlateau()[10][10] = 0;
+	myGoban->getPlateau()[9][10] = 1;
+	myGoban->getPlateau()[10][9] = 0;
+	myGoban->getPlateau()[9][9] = 1;
+	myGoban->getPlateau()[10][8] = 1;
+	EXPECT_EQ(myGoban->detecterLiberte(10,10), false);
+}
+
+TEST(ObjectTestSuicide, TestSuicide) {
+  goban *myGoban = new goban(10);
+	EXPECT_EQ(myGoban->detecterSuicide(5,5), true);
+	myGoban->getPlateau()[9][9] = 1;
+	EXPECT_EQ(myGoban->detecterSuicide(9,9), false);
+}
+
+TEST(ObjetTestSupprimer, testSupprimerRien) {
+	goban *myGoban = new goban(10);
+	myGoban->supprimerGroupe(5,5);
 }
