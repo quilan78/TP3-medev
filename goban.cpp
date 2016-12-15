db5589c05582a79b3ceb6b156386ceb5ab95cbae
David Tuckey <quilan.v@gmail.com>
2016-12-15 08:42:04 +0100

    Debugage disparition code

--- goban.cpp
+++ goban.cpp
@@ -15,7 +15,7 @@ void goban::jouer(int x,int y)
 	if (plateau[x][y] != -1) // Si la case n'est pas vide
 	{
 		int x2,y2;
-		cout<<"Case déjà occupée,rejouez"<<endl;
+		cout<<"Case dï¿½jï¿½ occupï¿½e,rejouez"<<endl;
 		cout<<"nouvelle valeur de x:"<<endl;
 		cin>>x2;
 		cout<<endl<<"nouvelle valeur de y:"<<endl;
@@ -73,4 +73,82 @@ void goban::afficher() {
 		std::cout << std::endl << std::endl;
 	}
 	std::cout << "C'est au joueur " << ((joueur) ? "blanc" : "noir") << " de jouer." << std::endl << std::endl << std::endl;
+}
+
+goban::goban( int _taille) {
+  taille = _taille;
+  joueur =0;
+  capture[0]=0;
+  capture[1]=0;
+
+  // Initialisation du plateau
+  for( int i=0; i<taille, i++) {
+    vector<int> temp;
+    plateau.push_back(temp);
+    for ( int u=0; u<taille, u++) {
+      plateau[i].push_back(-1);
+    }
+  }
+}
+
+bool goban::detecterLiberte( int x, int y ) {
+  int couleur = plateau[x][y];
+  bool liberte = false;
+  Int2 i;
+  i.i[0] = x;
+  i.i[1] = y;
+  checked.push_back(i);
+  if ( x > 0) {
+    if ( plateau[x-1][y] == -1 ) {
+      liberte = true;
+    }
+    else if ( plateau[x-1][y] == couleur and isNotChecked(x-1,y)) {
+      liberte = liberte||detecterLiberte(x-1, y);
+    }
+  }
+  if ( y > 0) {
+    if ( plateau[x][y-1] == -1 ) {
+      liberte = true;
+    }
+    else if ( plateau[x][y-1] == couleur and isNotChecked(x,y-1)) {
+      liberte = liberte||detecterLiberte(x, y-1);
+    }
+  }
+  if ( x < taille-1) {
+    if ( plateau[x+1][y] == -1 ) {
+      liberte = true;
+    }
+    else if ( plateau[x+1][y] == couleur and isNotChecked(x+1,y)) {
+      liberte = liberte||detecterLiberte(x+1, y);
+    }
+  }
+  if ( y < taille-1) {
+    if ( plateau[x][y+1] == -1 ) {
+      liberte = true;
+    }
+    else if ( plateau[x][y+1] == couleur and isNotChecked(x,y+1)) {
+      liberte = liberte||detecterLiberte(x, y+1);
+    }
+  }
+
+  return liberte;
+
+}
+
+bool goban::isNotChecked(int x, int y) {
+  bool retour = true;
+  for ( int i=0; i<checked.size(); i++) {
+    if ( checked[i].i[0] == x and checked[i].i[1] == y) {
+      retour = false;
+    }
+  }
+  return retour;
+
+}
+
+bool goban::detecterSuicide( int x, int y ) {
+  if ( plateau[x][y] == -1)
+    return true;
+  else
+    return false;
 }
---
+++ main_test.cpp
@@ -0,0 +1,26 @@
+#include "goban.h"
+#include <gtest>
+int main() {
+    goban *myGoban = new goban(10);
+    myGoban->afficher();
+    myGoban->jouer(5,5);
+    myGoban->afficher();
+    return 0;
+}
+
+
+TEST(ObjectTest, TestInitialisation) {
+  goban *myGoban = new goban(10);
+  EXPECT_EQ(myGoban->getTaille(), 10);
+  EXPECT_EQ(myGoban->getPlayer(), 0);
+  EXPECT_EQ(myGoban->getCapture()[0], 0);
+  EXPECT_EQ(myGoban->getCapture()[1], 0);
+
+  for( int i=0; i<myGoban->getTaille(); i++) {
+    vector<int> temp;
+    plateau.push_back(temp);
+    for ( int u=0; u<taille; u++) {
+      plateau[i].push_back(-1);
+    }
+  }
+}
