2a939fe05c6fd7df67bafde086892e7cbcebe95d
Jaimepastravailler <julien.wantz@yahoo.fr>
2016-12-15 10:46:42 +0100

    Nouveaux tests

--- main_test_2.cpp
+++ main_test_2.cpp
@@ -20,18 +20,19 @@ TEST (TestJouer,JoueursPosentPion){
     myGoban->setJoueur(0);
     myGoban->jouer(2,3);
     EXPECT_EQ(myGoban->getCase(2,3),0);
-    myGoban->setJoueur(1);
+    EXPECT_EQ(myGoban->getJoueur(),1);
     myGoban->jouer(5,8);
     EXPECT_EQ(myGoban->getCase(5,8),1);
+    EXPECT_EQ(myGoban->getJoueur(),0);
 }
 
 TEST (TestJouer,CaseOccupée){
     goban * myGoban = new goban(10);
     myGoban->setJoueur(0);
     myGoban->jouer(2,3);
-    myGoban->setJoueur(1);
     myGoban->jouer(2,3);
     EXPECT_EQ(myGoban->getCase(2,3),0);
+    EXPECT_EQ(myGoban->getJoueur(),1);
 }
 
 TEST (TestJouer,SupprimerGroupe){
@@ -40,10 +41,38 @@ TEST (TestJouer,SupprimerGroupe){
     myGoban->jouer(1,1);
     myGoban->setJoueur(0);
     myGoban->jouer(1,0);
+    myGoban->setJoueur(0);
     myGoban->jouer(2,1);
+    myGoban->setJoueur(0);
     myGoban->jouer(1,2);
+    myGoban->setJoueur(0);
     myGoban->jouer(0,1);
     EXPECT_EQ(myGoban->getCase(1,1),-1);
+}
+
+TEST (TestJouer,HorsPlateau){
+    goban * myGoban = new goban(10);
+    myGoban->jouer(11,0);
+    for (int i=0;i<myGoban->getTaille();i++){
+        for (int j=0;j<myGoban->getTaille();j++))
+        {
+            EXPECT_EQ(myGoban->getCase(i,j),-1);
+        }
+    }
+}
+
+TEST (TestJouer,TestSuicide){
+    goban * myGoban = new goban(10);
+    myGoban->jouer(1,0);
+    myGoban->setJoueur(0);
+    myGoban->jouer(2,1);
+    myGoban->setJoueur(0);
+    myGoban->jouer(1,2);
+    myGoban->setJoueur(0);
+    myGoban->jouer(0,1);
+    myGoban->jouer(1,1);
+    EXPECT_EQ(myGoban->getCase(1,1),-1);
+    EXPECT_EQ(myGoban->getJoueur(),1);
 }
 
 
